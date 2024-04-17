f0686e239a1133839f2dec977e4ebfec3c52d53d
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-04-18 08:58:05 +1000

    fix current date and time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { formattedDate, formattedTime } from "../constants";
 
 const Home = () => {
-  const date = formattedDate;
-  const time = formattedTime;
+  const now = new Date();
+  const date = new Intl.DateTimeFormat("en-AU", { dateStyle: "full" }).format(
+    now
+  );
+  const time = now.toLocaleTimeString("en-AU", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
@@ -12,7 +17,9 @@ const Home = () => {
             Upcoming Meeting at: 12.30 PM
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <h1 className="text-4xl font-extrabold lg:text-7xl uppercase">
+              {time}
+            </h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
